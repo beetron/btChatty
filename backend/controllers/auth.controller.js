575e97feef8f69de7ec2b0453a94b3704dfeecb0
Brian <bdev@btro.jp>
2024-06-13 21:26:07 +0900

    Auth updates

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,10 +2,12 @@ import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import generateTokenAndSetCookie from "../utility/generateToken.js";
 
+// SIGNUP
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword } = req.body;
 
+    // Check if user chosen passwords match
     if (password != confirmPassword) {
       return res.status(400).json({ error: "Password mismatch" });
     }
@@ -52,12 +54,48 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login");
-  console.log("login");
+// LOGIN
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(req.body);
+
+    // search for username
+    const user = await User.findOne({ username });
+
+    // check passwored integrity
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    // check credentials
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid login information" });
+    }
+
+    // generate token
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePhoto: user.profilePhoto,
+    });
+  } catch (error) {
+    console.log("Error during login", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
+//LOGOUT
 export const logout = (req, res) => {
-  res.send("logout");
-  console.log("logout");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "You have logged out" });
+  } catch (error) {
+    console.log("Error logging out", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
--- backend/utility/generateToken.js
+++ backend/utility/generateToken.js
@@ -9,6 +9,7 @@ const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days to milliseconds
     httpOnly: true, // counter Cross-Site scripting (XSS) attacks
     sameSite: "strict", // counter Cross-Site Request Forgery (CSRF)
+    secure: process.env.NODE_ENV !== "development",
   });
 };
 
