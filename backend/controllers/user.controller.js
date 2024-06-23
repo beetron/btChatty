919350c0d39068ce13062ea6f47bf7aa67ce1636
Brian <bdev@btro.jp>
2024-06-23 21:29:06 +0900

    Simplified code

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,15 +3,7 @@ import User from "../models/user.model.js";
 // Get user friend list
 export const getFriendList = async (req, res) => {
   try {
-    const userId = req.user._id;
-
-    // Find user object
-    const user = await User.findById(userId);
-
-    // Throw error if user not found
-    if (!user) {
-      return res.status(404).json({ error: "Error in user controller" });
-    }
+    const user = req.user;
 
     // Get friend list
     const friendList = await User.find({
@@ -30,18 +22,13 @@ export const getFriendList = async (req, res) => {
   }
 };
 
+// Block user
+// export const blockUser = async (req, res) => {};
+
 // Get pending user friend requests
 export const getFriendRequests = async (req, res) => {
   try {
-    const userId = req.user._id;
-
-    // Find user object
-    const user = await User.findById(userId);
-
-    // Throw error if user not found
-    if (!user) {
-      return res.status(404).json({ error: "Error in user controller" });
-    }
+    const user = req.user;
 
     // Get friend requests
     const friendRequests = user.friendRequests;
@@ -55,16 +42,7 @@ export const getFriendRequests = async (req, res) => {
 // Add friend request
 export const addFriendRequest = async (req, res) => {
   try {
-    const requesterId = req.user._id;
-
-    // Find requesting user
-    const requester = await User.findById(requesterId);
-
-    // Throw error if requesting user not found
-    if (!requester) {
-      console.log({ error: "Requesting user not found" });
-      return res.status(404).json({ error: "Error in user controller" });
-    }
+    const user = req.user;
 
     // Retrieve uniqueId from request params
     const { uniqueId } = req.params;
@@ -78,13 +56,19 @@ export const addFriendRequest = async (req, res) => {
       return res.status(400).json({ error: "User not found" });
     }
 
+    // Check if user it not adding itself
+    if (requestReceiver._id.equals(user._id)) {
+      console.log({ error: "Cannot add yourself" });
+      return res.status(400).json({ error: "Cannot add yourself" });
+    }
+
     // Check if user is already pending add request
-    if (requestReceiver.friendRequests.includes(requester._id)) {
+    if (requestReceiver.friendRequests.includes(user._id)) {
       console.log({ error: "Friend request already sent" });
       return res.status(400).json({ error: "Friend request already sent" });
     }
 
-    requestReceiver.friendRequests.push(requesterId);
+    requestReceiver.friendRequests.push(user._id);
     await requestReceiver.save();
 
     res.status(201).json({ message: "Friend request sent" });
