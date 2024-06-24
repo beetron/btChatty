b0435152abc55ed4d0d53406ded8a1f73a063e86
Brian <bdev@btro.jp>
2024-06-24 20:26:25 +0900

    wip

---
+++ frontend/src/components/sidebar/Messages.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Message from "./Message";
+
+const Messages = () => {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+
+export default Messages;
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 const SearchInput = () => {
   return (
@@ -9,10 +10,27 @@ const SearchInput = () => {
         className="w-full input input-bordered rounded-full"
       />
       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
-        Icon
+        <FiSearch className="w-5 h-5 outline-none" />
       </button>
     </form>
   );
 };
 
 export default SearchInput;
+
+// const SearchInput = () => {
+//   return (
+//     <form className="flex items-center gap-2">
+//       <input
+//         type="text"
+//         placeholder="Search messages..."
+//         className="w-full input input-bordered rounded-full"
+//       />
+//       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+//         <FiSearch className="w-5 h-5 outline-none" />
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default SearchInput;
