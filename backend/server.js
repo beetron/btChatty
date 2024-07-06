2863387b19e5d9d0f1145eda13b0be3fdf1f73cf
Brian <bdev@btro.jp>
2024-07-06 20:16:23 +0900

    wip

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,10 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 dotenv.config(); // Init default dotenv
+
+const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
