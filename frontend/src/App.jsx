fe3f2262905bdaf2d7b57059885f3c04f2455604
Brian <bdev@btro.jp>
2024-07-06 22:29:46 +0900

    AuthContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={authUser ? <Home /> : <Login />} />
+        <Route path="/login" element={authUser ? <Home /> : <Login />} />
+        <Route path="/signup" element={authUser ? <Home /> : <SignUp />} />
       </Routes>
       <Toaster />
     </div>
