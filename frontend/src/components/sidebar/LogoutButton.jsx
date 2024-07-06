80d5cc209444482bb16790465ae99f745e6996da
Brian <bdev@btro.jp>
2024-07-06 22:30:55 +0900

    added useLogout hook

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,9 +1,15 @@
 import { MdLogout } from "react-icons/md";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
+  const { loading, logout } = useLogout();
+
   return (
     <div className="mt-auto">
-      <MdLogout className="w-6 h-6 text-white cursor-pointer" />
+      <MdLogout
+        className="w-6 h-6 text-white cursor-pointer"
+        onClick={logout}
+      />
     </div>
   );
 };
