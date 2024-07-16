0246c31fd9dc72a3d2395cdf8000c70318dd356a
Brian <bdev@btro.jp>
2024-07-16 18:33:02 +0900

    Cleaned code

--- frontend/src/components/sidebar/Friend.jsx
+++ frontend/src/components/sidebar/Friend.jsx
@@ -4,21 +4,16 @@ import friendStore from "../../store/friendStore";
 const Friend = ({ friend }) => {
   const { selectedFriend, setSelectedFriend } = friendStore();
 
-  // Handle selected state
-  const handleSelected = () => {
-    setSelectedFriend(friend);
-  };
-
   // Double check selected friend
-  const isSelected = selectedFriend === friend;
+  const isSelected = selectedFriend?._id === friend._id;
 
   return (
     <>
       <div
-        className={`flex gap-2 items-center rounded p-2 py-1 cursor-pointer ${
-          isSelected ? "bg-sky-500" : "hover:bg-sky-500"
+        className={`flex gap-2 items-center rounded p-2 py-1 cursor-pointer hover:bg-gray-600 ${
+          isSelected ? "bg-sky-500" : ""
         }`}
-        onClick={handleSelected}
+        onClick={() => setSelectedFriend(friend)}
       >
         <div className="w-12 h-12 bg-gray-300 rounded-full overflow-hidden">
           <img src={friend.profilePhoto} className="w-full h-full" />
