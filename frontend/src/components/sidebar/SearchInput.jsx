0651e2542b79e7b0e968730ce208e5e5b4e6287c
Brian <bdev@btro.jp>
2024-07-17 22:06:36 +0900

    Update SearchInput.jsx

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { toast } from "react-hot-toast";
+import friendStore from "../../store/friendStore";
+import useGetFriends from "../../hooks/useGetFriends";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedFriend } = friendStore();
+  const { friends } = useGetFriends();
+
+  const submitSearch = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      setSearch("");
+      return toast.error("Search query must be at least 3 characters long.");
+    }
+    const friend = friends.find((c) =>
+      c.nickname.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (friend) {
+      setSelectedFriend(friend);
+      setSearch("");
+    } else {
+      toast.error("Friend not found.");
+      setSearch("");
+    }
+  };
+
   return (
-    <form className="flex items-center gap-2">
+    <form onSubmit={submitSearch} className="flex items-center gap-2">
       <input
         type="text"
-        placeholder="Search messages..."
+        placeholder="Search friend..."
         className="w-full input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
         <FiSearch className="w-5 h-5 outline-none" />
@@ -17,20 +46,3 @@ const SearchInput = () => {
 };
 
 export default SearchInput;
-
-// const SearchInput = () => {
-//   return (
-//     <form className="flex items-center gap-2">
-//       <input
-//         type="text"
-//         placeholder="Search messages..."
-//         className="w-full input input-bordered rounded-full"
-//       />
-//       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
-//         <FiSearch className="w-5 h-5 outline-none" />
-//       </button>
-//     </form>
-//   );
-// };
-
-// export default SearchInput;
