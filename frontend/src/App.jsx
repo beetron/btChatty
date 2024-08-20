4a1294f4e4de178281dd7b86efc32b011655314d
Brian <bdev@btro.jp>
2024-08-20 17:39:59 +0900

    Added PWA support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import SignUp from "./pages/signup/SignUp";
 function App() {
   const { authUser } = useAuthContext();
   return (
-    <div className="p-4 h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center">
       <Routes>
         <Route path="/" element={authUser ? <Home /> : <Login />} />
         <Route path="/login" element={authUser ? <Home /> : <Login />} />
