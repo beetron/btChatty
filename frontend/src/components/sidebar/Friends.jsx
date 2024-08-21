433b12333ba0d10a934d68db1e9482dd7f1ebaee
Brian <bdev@btro.jp>
2024-08-21 22:26:10 +0900

    Added SettingsButton component

--- frontend/src/components/sidebar/Friends.jsx
+++ frontend/src/components/sidebar/Friends.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import SearchInput from "./SearchInput";
 import FriendList from "./FriendList";
 import LogoutButton from "./LogoutButton";
+import SettingsButton from "./SettingsButton";
 
 const Friends = () => {
   return (
     <div className="w-full max-w-none flex flex-col items-start mt-4">
-      <LogoutButton />
+      <div className="grid gap-4 grid-cols-3">
+        <SettingsButton />
+        <LogoutButton />
+      </div>
       <div className="divider px-3"></div>
       <SearchInput />
       <div className="divider px-3"></div>
---
+++ frontend/src/components/sidebar/SettingsButton.jsx
@@ -0,0 +1,11 @@
+import { MdSettings } from "react-icons/md";
+
+const SettingsButton = () => {
+  return (
+    <div className="mt-auto">
+      <MdSettings className="w-8 h-8 text-white cursor-pointer" />
+    </div>
+  );
+};
+
+export default SettingsButton;
