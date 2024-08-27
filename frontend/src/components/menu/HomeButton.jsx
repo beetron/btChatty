fff4288fcc3314bdf46b72b85df52be14cd3520e
Brian <bdev@btro.jp>
2024-08-27 23:53:31 +0900

    Added HomeButton function

--- frontend/src/components/menu/HomeButton.jsx
+++ frontend/src/components/menu/HomeButton.jsx
@@ -1,9 +1,19 @@
 import { IoReturnUpBackOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const HomeButton = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className="mt-auto">
-      <IoReturnUpBackOutline className="w-8 h-8 text-white cursor-pointer" />
+      <IoReturnUpBackOutline
+        className="w-8 h-8 text-white cursor-pointer"
+        onClick={handleClick}
+      />
     </div>
   );
 };
