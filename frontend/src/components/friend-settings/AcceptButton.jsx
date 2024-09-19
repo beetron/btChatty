9cc51cc68c6b387240056524b82aa18d71291b7d
Brian <bdev@btro.jp>
2024-09-19 23:22:31 +0900

    Removed unused code

--- frontend/src/components/friend-settings/AcceptButton.jsx
+++ frontend/src/components/friend-settings/AcceptButton.jsx
@@ -2,9 +2,9 @@ import { LiaThumbsUp } from "react-icons/lia";
 import useAcceptFriendRequest from "../../hooks/useAcceptFriendRequest";
 import useFriendStore from "../../store/friendStore";
 
-const AcceptButton = ({ friendRequest, onUpdate }) => {
+const AcceptButton = ({ friendRequest }) => {
   const { loading, acceptFriendRequest } = useAcceptFriendRequest();
-  const { render, setRender } = useFriendStore();
+  const { setRender } = useFriendStore();
 
   const handleClick = async () => {
     await acceptFriendRequest(friendRequest.uniqueId);
