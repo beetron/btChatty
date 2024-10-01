d2f94febd38df069f7d04ecaf6bc58d085cd3ba0
Brian <bdev@btro.jp>
2024-10-01 22:25:59 +0900

    Removed unused code

--- frontend/src/components/messages/MessagesContainer.jsx
+++ frontend/src/components/messages/MessagesContainer.jsx
@@ -38,20 +38,3 @@ const MessagesContainer = () => {
 };
 
 export default MessagesContainer;
-
-const NoFriendSelected = () => {
-  const { authUser } = useAuthContext();
-
-  return (
-    <div className="flex items-center justify-center w-full h-full">
-      <div
-        className="px-4 text-center sm:text-lg md:text-xl 
-      text-gray-200 font-semibold flex flex-col items-center gap-2"
-      >
-        <p>{`Hi, ${authUser.nickname}. `}</p>
-        <p>Welcome message or hint/tips</p>
-        <p>How to use app here</p>
-      </div>
-    </div>
-  );
-};
