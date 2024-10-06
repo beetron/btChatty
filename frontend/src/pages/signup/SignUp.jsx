2a4f4c2d1a770f832725a9199084135ac531bb56
Brian <bdev@btro.jp>
2024-10-06 22:58:26 +0900

    Updated to force lower case username

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -27,9 +27,7 @@ const SignUp = () => {
         <form onSubmit={submitForm}>
           <div>
             <label className="label">
-              <span className="text-base label-text">
-                Username (case sensitive)
-              </span>
+              <span className="text-base label-text">Username</span>
             </label>
             <input
               type="text"
@@ -37,7 +35,7 @@ const SignUp = () => {
               className="w-full input input-bordered h-10"
               value={inputs.username}
               onChange={(e) =>
-                setInputs({ ...inputs, username: e.target.value })
+                setInputs({ ...inputs, username: e.target.value.toLowerCase() })
               }
             />
           </div>
@@ -108,60 +106,3 @@ const SignUp = () => {
 };
 
 export default SignUp;
-
-// const SignUp = () => {
-//   return (
-//     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-//       <div className="w-full p-6 rounded-lg bg-gray-400 shadow-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20">
-//         <h1 className="text-3xl font-semibold text-center text-gray-300">
-//           Sign up for <span className="text-blue-400">btChatty</span>
-//         </h1>
-//         <form>
-//           <div>
-//             <label className="label">
-//               <span className="text-base label-text">Username</span>
-//             </label>
-//             <input
-//               type="text"
-//               placeholder=""
-//               className="w-full input input-bordered h-10"
-//             />
-//           </div>
-//           <div>
-//             <label className="label">
-//               <span className="text-base label-text">Password</span>
-//             </label>
-//             <input
-//               type="password"
-//               placeholder="Must be at least 6 characters"
-//               className="w-full input input-bordered h-10"
-//             />
-//           </div>
-//           <div>
-//             <label className="label">
-//               <span className="text-base label-text">Confirm password</span>
-//             </label>
-//             <input
-//               type="password"
-//               placeholder="Must be at least 6 characters"
-//               className="w-full input input-bordered h-10"
-//             />
-//             <a
-//               href="#"
-//               className="text-sm hover:underline hover:text-blue-400 mt-4 inline-block"
-//             >
-//               Already have an account?
-//             </a>
-//           </div>
-//           <div>
-//             <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-//               Sign Up
-//             </button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SignUp;
