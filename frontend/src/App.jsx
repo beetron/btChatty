e7dbcbef865fa470dee56b4f1103b208931f27c3
Brian <bdev@btro.jp>
2024-10-14 20:40:02 +0900

    Removed the wrapping of notification init around App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 import { useEffect } from "react";
-import OneSignal from "react-onesignal";
+// import OneSignal from "react-onesignal";
 
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -14,13 +14,13 @@ import FriendSettings from "./pages/friend-settings/FriendSettings";
 
 function App() {
   // OneSignal initialization for notifications
-  useEffect(() => {
-    (async () => {
-      OneSignal.init({
-        appId: import.meta.env.VITE_ONESIGNAL_APP_ID,
-      });
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     OneSignal.init({
+  //       appId: import.meta.env.VITE_ONESIGNAL_APP_ID,
+  //     });
+  //   })();
+  // }, []);
 
   const { authUser } = useAuthContext();
 
@@ -30,8 +30,14 @@ function App() {
         <Route path="/" element={authUser ? <Home /> : <Login />} />
         <Route path="/messages" element={authUser ? <Messages /> : <Login />} />
         <Route path="/settings" element={authUser ? <Settings /> : <Login />} />
-        <Route path="/login" element={authUser ? <Home /> : <Login />} />
-        <Route path="/signup" element={authUser ? <Home /> : <SignUp />} />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
         <Route
           path="/friend-settings"
           element={authUser ? <FriendSettings /> : <Login />}
