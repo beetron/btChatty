c8126d71d55ac7390cf07a0700542bd6d6dea4fc
Brian <bdev@btro.jp>
2024-10-15 19:08:52 +0900

    Updated to register user _id as OneSignal's external_id

--- frontend/src/services/runOneSignal.js
+++ frontend/src/services/runOneSignal.js
@@ -1,9 +1,15 @@
 import OneSignal from "react-onesignal";
 
 export default async function runOneSignal() {
+  // Retrieve user _id for OneSignal's external_id linking
+  const authUser = JSON.parse(localStorage.getItem("btchatty-user"));
+
+  // Initialize OneSignal
   await OneSignal.init({
     appId: import.meta.env.VITE_ONESIGNAL_APP_ID,
     allowLocalhostAsSecureOrigin: true,
   });
-  console.log("OneSignal initialized");
+
+  // Register or login with external_id (_id used)
+  OneSignal.login(authUser._id);
 }
